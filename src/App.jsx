b489905ca9730b7a117aebfec332513e6405448d
Umutai <umutaiaitmamativa@gmail.com>
2022-11-12 23:39:21 -1200

    5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 // import SvgLogo from './assets/img/pizza-logo.svg';
 import { Header } from "./components";
-import {Home, Cart} from './pages';
-
+import { Home, Cart } from "./pages";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3000/db.json").then((resp) => resp.json()).then(json => {
+      setPizzas(json.pizzas)
+    });
+  }, []);
+  console.log(pizzas);
 
   return (
     <div className="App">
@@ -15,8 +22,8 @@ function App() {
         <div className="content">
           <Routes>
             <Route>
-            <Route path='/' element={<Home/>} exact/>
-        <Route path='/cart' element={<Cart/>} exact/>
+              <Route path="/" element={<Home />} exact />
+              <Route path="/cart" element={<Cart />} exact />
             </Route>
           </Routes>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -59,5 +59,5 @@ const Header = () => {
             </div>
         </div>
     );
-};
+};       
 export default Header;
